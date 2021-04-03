64463f7d2a590326927ddd0e4a32ab4178974633
Sophie Sun <sophiesun@Sophies-MacBook-Pro.local>
2021-04-03 00:18:39 -0400

    DONEEEEE

--- main.c
+++ main.c
@@ -37,6 +37,33 @@ int main(void){
 
 
     return 0;
+    // struct rgb_img *im;
+    // struct rgb_img *cur_im;
+    // struct rgb_img *grad;
+    // double *best;
+    // int *path;
+
+    // read_in_img(&im, "HJoceanSmall.bin");
+    
+    // for(int i = 0; i < 5; i++){
+    //     printf("i = %d\n", i);
+    //     calc_energy(im,  &grad);
+    //     dynamic_seam(grad, &best);
+    //     recover_path(best, grad->height, grad->width, &path);
+    //     remove_seam(im, &cur_im, path);
+
+    //     char filename[200];
+    //     sprintf(filename, "img%d.bin", i);
+    //     write_img(cur_im, filename);
+
+
+    //     destroy_image(im);
+    //     destroy_image(grad);
+    //     free(best);
+    //     free(path);
+    //     im = cur_im;
+    // }
+    // destroy_image(im);
 }
 
 //gcc main.c seamcarving.c c_img.c -o main
